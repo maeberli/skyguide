208c3eb1f04da82e684cc189134df1c21c73df60
marco.aeberli@gmail.com <marco.aeberli@gmail.com@a60c681c-5de4-430e-ccf0-69f4e65b53dd>
2012-08-31 08:40:37 +0000

    -added example doc-style to skylogger.h and skylogger.cpp

--- trunk/Projet/SkyGuide/src/skylogger.cpp
+++ trunk/Projet/SkyGuide/src/skylogger.cpp
@@ -1,23 +1,34 @@
+/**
+  * Class implementation file of SkyLogger
+  *
+  * \author Marco Aeberli
+  *
+  * \copyright Project P1 group DLM14 2012, all rights reserved
+  */
 #include "skylogger.h"
 
 #include <QDebug>
 #include <QTime>
 #include <QtCore/QCoreApplication>
 
-#define PREFIX QTime::currentTime().toString("hh:mm:ss.zzz")
+#define PREFIX QTime::currentTime().toString("hh:mm:ss.zzz")    //< Base prefix for logging message to console, file, etc. ...
 #define FILENAME "SkyGuide.log"
 
 
 SKYLOGGER::SkyLogger::SkyLogger(QObject *parent) :
     QObject(parent)
 {
-
+    //open/create the file to log.
     this->p_file = new QFile(QCoreApplication::applicationDirPath()+"/"+FILENAME);
 
     if(p_file->open(QIODevice::Append |  QIODevice::Text))
+    {
         this->p_fileOut = new QTextStream(p_file);
-    else
+    }
+    else // possible if no rights, no space left, ...
+    {
         this->consoleLog(ERROR, tr("Couldn't open file for log messages."));
+    }
 
 }
 
@@ -27,14 +38,14 @@ SKYLOGGER::SkyLogger::~SkyLogger()
     delete p_file;
 }
 
-void SKYLOGGER::SkyLogger::logMessage(SKYLOGGER::SkyLoggerTypes type, const QString & message)
+void SKYLOGGER::SkyLogger::logMessage(SKYLOGGER::SkyLoggerTypes type, const QString& message)
 {
     this->consoleLog(type, message);
     this->fileLog(type, message);
     this->emitSignals(type, message);
 }
 
-void SKYLOGGER::SkyLogger::consoleLog(SKYLOGGER::SkyLoggerTypes type, const QString & message)
+void SKYLOGGER::SkyLogger::consoleLog(SKYLOGGER::SkyLoggerTypes type, const QString& message)
 {
     switch(type)
     {
@@ -61,7 +72,7 @@ void SKYLOGGER::SkyLogger::consoleLog(SKYLOGGER::SkyLoggerTypes type, const QStr
     }
 }
 
-void SKYLOGGER::SkyLogger::fileLog(SKYLOGGER::SkyLoggerTypes type, const QString & message)
+void SKYLOGGER::SkyLogger::fileLog(SKYLOGGER::SkyLoggerTypes type, const QString& message)
 {
     if(p_fileOut->status() ==  QTextStream::Ok)
     {
@@ -95,7 +106,7 @@ void SKYLOGGER::SkyLogger::fileLog(SKYLOGGER::SkyLoggerTypes type, const QString
     }
 }
 
-void SKYLOGGER::SkyLogger::emitSignals(SKYLOGGER::SkyLoggerTypes type, const QString &message)
+void SKYLOGGER::SkyLogger::emitSignals(SKYLOGGER::SkyLoggerTypes type, const QString& message)
 {
     switch(type)
     {
@@ -110,6 +121,7 @@ void SKYLOGGER::SkyLogger::emitSignals(SKYLOGGER::SkyLoggerTypes type, const QSt
         break;
     case SKYLOGGER::VERBOSE:
     case SKYLOGGER::DEBUG:
+    default:
         break;
     }
 }
--- trunk/Projet/SkyGuide/src/skylogger.h
+++ trunk/Projet/SkyGuide/src/skylogger.h
@@ -1,37 +1,102 @@
+/**
+  * Class declaration file of SkyLogger
+  *
+  * \author Marco Aeberli
+  *
+  * \copyright Project P1 group DLM14 2012, all rights reserved
+  */
+
 #ifndef SKYLOGGER_H
 #define SKYLOGGER_H
 
+//include Qt-headers
 #include <QObject>
 #include <QFile>
 #include <QTextStream>
 
+//include SkyGuide specific headers
 #include "skyloggertypes.h"
 
 namespace SKYLOGGER
 {
 
+/**
+  * Class SkyLogger implements the fonctionality for logging information on console and in file.
+  */
 class SkyLogger : public QObject
 {
     Q_OBJECT
 public:
+    /**
+      * Base constructor. Initialize the object.
+      *
+      * @param parent   parent object.
+      */
     explicit SkyLogger(QObject *parent = 0);
+
+    /**
+      * Destructor, do the clean up.
+      */
     ~SkyLogger();
 
 signals:
-    void receivedError(const QString & message);
-    void receivedWarning(const QString & message);
-    void receivedInfo(const QString & message);
+    /**
+      * Signal should be emited when object of SkyLogger has to log a message of type ERROR.
+      *
+      * @param message  received error message.
+      */
+    void receivedError(const QString& message);
+
+    /**
+      * Signal should be emited when object of SkyLogger has to log a message of type WARNING.
+      *
+      * @param message  received warning message.
+      */
+    void receivedWarning(const QString& message);
+
+    /**
+      * Signal should be emited when object of SkylOgger has to log a message of type INFO.
+      *
+      * @param message  received info message.
+      */
+    void receivedInfo(const QString& message);
 
 public slots:
-    void logMessage(SKYLOGGER::SkyLoggerTypes type, const QString & message);
+    /**
+      * Methode which handels the logging of a message, diferenced by type.
+      *
+      * @param type     Defines the type of the message to log.
+      * @param message  Message text to log.
+      */
+    void logMessage(SKYLOGGER::SkyLoggerTypes type, const QString& message);
 
 private:
-    void consoleLog(SKYLOGGER::SkyLoggerTypes type, const QString & message);
-    void fileLog(SKYLOGGER::SkyLoggerTypes type, const QString & message);
-    void emitSignals(SKYLOGGER::SkyLoggerTypes type, const QString & message);
+    /**
+      * Methode writes a log message to the console.
+      *
+      * @param type     Defines the type of the message to log.
+      * @param message  Message text to log.
+      */
+    void consoleLog(SKYLOGGER::SkyLoggerTypes type, const QString& message);
 
-    QFile* p_file;
-    QTextStream* p_fileOut;
+    /**
+      * Methode writes a log message to a logfile.
+      *
+      * @param type     Defines the type of the message to log.
+      * @param message  Message text to log.
+      */
+    void fileLog(SKYLOGGER::SkyLoggerTypes type, const QString& message);
+
+    /**
+      * Methode emits the to the type associeted signal if neccessaire.
+      *
+      * @param type     Defines the type of the log message.
+      * @param message  Message text to log.
+      */
+    void emitSignals(SKYLOGGER::SkyLoggerTypes type, const QString& message);
+
+    QFile* p_file;          //< log file object.
+    QTextStream* p_fileOut; //< textstream object to write to the log file.
 };
 
 }
