a67fa5349645cfd98e0371f9cfd297fd6aeaa02c
matthieu.rossier@gmail.com <matthieu.rossier@gmail.com@a60c681c-5de4-430e-ccf0-69f4e65b53dd>
2013-06-01 17:00:50 +0000

    Commit DevMatthieu 01-06-2013

---
+++ ch/hearc/coursjava/meteofinal/use/remote/ListOfInterfaces.java
@@ -0,0 +1,45 @@
+package network;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListOfInterfaces {
+
+	public static List<NetworkInterface> getAllNetworkInterfaces() throws SocketException {
+		List<NetworkInterface> allNetworkInterfaces = new LinkedList<NetworkInterface>();
+
+		Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+		while (networkInterfaces.hasMoreElements()) {
+			NetworkInterface currentNetworkInterface = networkInterfaces.nextElement();
+
+			allNetworkInterfaces.add(currentNetworkInterface);
+		}
+
+		return allNetworkInterfaces;
+	}
+
+	public static List<InetAddress> getAllAdresses(NetworkInterface networkInterface) {
+		List<InetAddress> allAddresses = new LinkedList<InetAddress>();
+
+		Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+
+		if (addresses.hasMoreElements()) {
+			while (addresses.hasMoreElements()) {
+				InetAddress currentAddress = addresses.nextElement();
+
+				allAddresses.add(currentAddress);
+			}
+
+			return allAddresses;
+		} else {
+			return null;
+		}
+
+	}
+
+}
